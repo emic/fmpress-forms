ca9c3bf623d891dc2353b65480cbd23a86603fec
Atarime <suzuki@emic.co.jp>
2024-10-31 18:31:48 +0900

    Fix PHP namespace

--- includes/drivers/class-database.php
+++ includes/drivers/class-database.php
@@ -13,8 +13,8 @@ require_once ABSPATH . 'wp-admin/includes/file.php';
 require_once ABSPATH . 'wp-includes/sodium_compat/src/Core/Util.php';
 require_once ABSPATH . 'wp-includes/sodium_compat/src/Compat.php';
 
-use \WP_Http;
-use \WP_Error;
+use WP_Http;
+use WP_Error;
 
 /**
  * Driver for FileMaker Data API
